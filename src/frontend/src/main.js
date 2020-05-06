c9d630113308af75fa5be31aacd949e0ed88b2c8
Clément Besnier <clemsciences@aol.com>
2020-05-07 00:41:51 +0200

    Added bootstrap

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,5 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+// This imports all the layout components such as <b-container>, <b-row>, <b-col>:
+import { LayoutPlugin } from 'bootstrap-vue'
+Vue.use(LayoutPlugin)
+
+// This imports <b-modal> as well as the v-b-modal directive as a plugin:
+import { ModalPlugin } from 'bootstrap-vue'
+Vue.use(ModalPlugin)
+
+// This imports <b-card> along with all the <b-card-*> sub-components as a plugin:
+import { CardPlugin } from 'bootstrap-vue'
+Vue.use(CardPlugin)
+
+// This imports directive v-b-scrollspy as a plugin:
+import { VBScrollspyPlugin } from 'bootstrap-vue'
+Vue.use(VBScrollspyPlugin)
+
+// This imports the dropdown and table plugins
+import { DropdownPlugin, TablePlugin } from 'bootstrap-vue'
+Vue.use(DropdownPlugin)
+Vue.use(TablePlugin)
 
 Vue.config.productionTip = false
 
