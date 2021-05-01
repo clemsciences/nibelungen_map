256913b3aae82f9accc092b82e026518375a6556
Clément Besnier <clemsciences@aol.com>
2021-05-02 00:52:12 +0200

    Added CORS for production server

--- nibelungen_map_server/server_app.py
+++ nibelungen_map_server/server_app.py
@@ -2,8 +2,12 @@
 
 """
 
+from flask_cors import CORS
+
 from nibelungen_map_server import create_app
 from nibelungen_map_server.routes import nibelungen_map
 
 
 app = create_app(nibelungen_map, "")
+cors = CORS()
+cors.init_app(app)
